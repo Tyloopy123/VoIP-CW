0317b5e169aa17344f9972f2042e3fa56c6082b8
bolaji-onanuga <bobonanuga95@icloud.com>
2021-03-15 17:12:41 +0000

    Revision 1
    - Removed unnecessary files
    - Made changes to ThreadedSound classes

--- CW1/CW1.1/src/com/company/NetworkCoursework.java
+++ CW1/CW1.1/src/com/company/NetworkCoursework.java
@@ -24,7 +24,6 @@ public class NetworkCoursework {
     private static InetAddress clientIP;
 
     public enum SocketType {
-
         Type1, Type2, Type3, Type4;
     }
 
--- CW1/CW1.1/src/com/company/ThreadedSoundDuplex.java
+++ CW1/CW1.1/src/com/company/ThreadedSoundDuplex.java
@@ -1,14 +1,14 @@
 package com.company;
 
 /*
-@bobby onanuga 14/3/2021
+@bolaji onanuga 14/3/2021
  */
 
 public class ThreadedSoundDuplex {
 
     public  static void main(String[] args){
-        ThreadedSoundReceiver rec  = new ThreadedSoundReceiver();
-        ThreadedSoundSender sender = new ThreadedSoundSender();
+        ThreadedSoundReceiver rec  = new ThreadedSoundReceiver(NetworkCoursework.SocketType.Type1);
+        ThreadedSoundSender sender = new ThreadedSoundSender(NetworkCoursework.SocketType.Type1);
         rec.start();
         sender.start();
     }
